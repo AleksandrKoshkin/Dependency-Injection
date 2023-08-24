c013e2bece0dcaf692b534b52ada733ec0bde5b8
Aleksandr <aleksandr.koshkin.2000@mail.ru>
2023-08-24 11:01:44 +0300

    Four

---
+++ 05_di/java2/beans/src/main/java/ru/netology/MainServlet.java
@@ -0,0 +1,63 @@
+package ru.netology;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
+import ru.netology.controller.PostController;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+  public static final String API_POSTS = "/api/posts";
+  public static final String API_POSTS_D = "/api/posts/\\d+";
+  public static final String STR = "/";
+  public static final String GET = "GET";
+  public static final String POST = "POST";
+  public static final String DELETE = "DELETE";
+  private PostController controller;
+
+  @Override
+  public void init() {
+    final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+    controller = (PostController) context.getBean("postController");
+  }
+
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp) {
+
+    try {
+      final var path = req.getRequestURI();
+      final var method = req.getMethod();
+
+      if (method.equals(GET) && path.equals(API_POSTS)) {
+        controller.all(resp);
+        return;
+      }
+      if (method.equals(GET) && path.matches(API_POSTS_D)) {
+
+        final var id = parseId(path);
+        controller.getById(id, resp);
+        return;
+      }
+      if (method.equals(POST) && path.equals(API_POSTS)) {
+        controller.save(req.getReader(), resp);
+        return;
+      }
+      if (method.equals(DELETE) && path.matches(API_POSTS_D)) {
+
+        final var id = parseId(path);
+        controller.removeById(id, resp);
+        return;
+      }
+      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    } catch (Exception e) {
+      e.printStackTrace();
+      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  private long parseId(String path) {
+    return Long.parseLong(path.substring(path.lastIndexOf(STR)));
+  }
+}
